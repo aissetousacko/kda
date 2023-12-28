bfc89af8723a33d71590cb8a833d5be4e62ab7a5
Aïssetou SACKO <aissetou.sck@gmail.com>
2023-12-28 19:00:05 +0100

    Adding header and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import logo from './assets/logo_black.png'
+import darkIcon from './assets/icons/icon_dark.png'
+import arrowIcon from './assets/icons/icon_arrow.png'
+import bannerHome from './assets/banner_home.png'
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div className="home">
-      <h1>Hello World</h1>
-      <h2>WELCOME!</h2>
+      <div className="header">
+        <img src={logo} alt="Logo K/DA " className="header__img" />
+
+        <Navbar />
+
+        <img
+          src={darkIcon}
+          alt="Switch dark mode button"
+          className="switch-btn btn-moon"
+        />
+      </div>
+
+      <div className="home__banner">
+        <img src={bannerHome} alt="Banner Home page" />
+
+        <button className="listen">
+          Listen them now
+          <img src={arrowIcon} alt="Arrow icon" />
+        </button>
+      </div>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <ul className="navbar">
+      <li className="navbar__item active">Home</li>
+      <li className="navbar__item">Listen</li>
+      <li className="navbar__item">The Girls</li>
+      <li className="navbar__item">Follow</li>
+    </ul>
+  )
+}
+
+export default Navbar
