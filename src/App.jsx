8958ef5d7fe36402f82c28a4db0b811fda35dfd6
Aïssetou SACKO <aissetou.sck@gmail.com>
2024-01-09 19:01:55 +0100

    Adding footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+
+        <Footer />
       </BrowserRouter>
     </div>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer>
+      © 2023 - Made with love <br />
+      K/DA a été créé en vertu de la politique juridique Riot Games intitulée «
+      Jargon juridique » relative à l&apos;utilisation d&apos;actifs de Riot
+      Games. Riot Games ne soutient ni ne sponsorise ce projet
+    </footer>
+  )
+}
+
+export default Footer
