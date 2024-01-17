0df57b039a9602ab43ca2dea390b7d8890f10615
Aïssetou SACKO <aissetou.sck@gmail.com>
2024-01-17 12:54:03 +0100

    Adding dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
 import Navbar from './Navbar'
-import logo from '../assets/logo_black.png'
+import logoBlack from '../assets/logo_black.png'
+import logoWhite from '../assets/logo_white.png'
 import darkIcon from '../assets/icons/icon_dark.png'
+import lightIcon from '../assets/icons/icon_light.png'
+import { useContext } from 'react'
+import { DarkModeContext } from '../context/DarkModeContext'
 
 const Header = () => {
+  const { darkMode, toggle } = useContext(DarkModeContext)
+
   return (
     <div className="header">
-      <img src={logo} alt="Logo K/DA " className="header__logo" />
+      <img
+        src={darkMode ? logoWhite : logoBlack}
+        alt="Logo K/DA "
+        className="header__logo"
+      />
 
       <Navbar />
 
       <img
-        src={darkIcon}
+        src={darkMode ? lightIcon : darkIcon}
         alt="Switch dark mode button"
-        className="switch-btn btn-moon"
+        className="switch-btn"
+        onClick={toggle}
       />
     </div>
   )
